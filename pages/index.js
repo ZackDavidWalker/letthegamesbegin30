aa0242cb06ddabb40ce261fcd6bcf5aa0d56a8b5
Zack Walker <47477187+DavidZWalker@users.noreply.github.com>
2024-02-06 01:54:45 +0100

    working state

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,64 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+// pages/index.js
 
-export default function Home() {
+import { useState, useEffect } from 'react';
+import Content from '../components/Content'; // Import the Content component
+import styles from '../styles/styles.module.css'; // Import the CSS module
+
+const Home = () => {
+  const targetDate = new Date('2024-02-06T01:22:00'); // Set your desired end date and time
+  const [countdownText, setCountdownText] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const currentDate = new Date();
+      const timeDifference = targetDate - currentDate;
+
+      if (timeDifference <= 0) {
+        clearInterval(interval);
+        setCountdownText('');
+      } else {
+        const hours = Math.floor(timeDifference / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        const isFinalMinute = minutes === 0 && seconds <= 60;
+
+        setCountdownText(`${hours}h ${minutes}m ${seconds}s`);
+        document.getElementById('countdown').classList.toggle(styles.red, isFinalMinute);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [targetDate]);
+
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+      {countdownText !== '' && (
+        <>
+        <div>
+          <p className={styles.textAboveBelow}>
+            Wilkommen! Schön, dass ihr es schonmal bis hierher geschafft habt.
+            Das bedeutet, dass ihr das erste Rätsel schon gelöst habt. 
+            Das war aber nur ein kleiner Geschmack, um euch auf die bevorstehende Reise vorzubereiten.
+            Das Abenteuer beginnt in
+          </p>
+        </div>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+        <div id="countdown" className={styles.countdown}>
+          {countdownText}
+        </div>
 
-      <Footer />
+        <div>
+          <p className={styles.textAboveBelow}>Viel Erfolg!</p>
+        </div>
+          </>
+        )}
+
+        {countdownText === '' && <Content />} {/* Render Content component when countdown expires */}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
